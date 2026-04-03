ee606b2a5487566658ac71651681351bcde6e0ff
@CE/DE <adipthatal7777@gmail.com>
2026-04-03 22:45:42 +0530

    Add files via upload
    Logics

---
+++ 1-n print.py
@@ -0,0 +1,3 @@
+n = int(input("Enter your num size: "))
+for i in range(1, n + 1):
+    print(i)
---
+++ oddeven.py
@@ -0,0 +1,5 @@
+a = int(input("Enter your first num: "))
+if a % 2 == 0:
+    print("The", a, "num is even")
+else:
+    print("odd")
